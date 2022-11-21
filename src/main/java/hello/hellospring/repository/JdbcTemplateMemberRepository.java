533dbf0e018cd740fd738d81f0479c663d206227
jason <ypc1@naver.com>
2022-11-21 22:28:44 +0900

    jdbc template 방식

---
+++ src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,78 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+//jdbc 메뉴얼 참고
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate jdbcTemplate; // 템플릿 메서드 패턴이 들어가있다
+
+    // 생성자가 1개면 @Autowired 생략 가능  스프링이 자동으로 인젝션 해주므로
+    @Autowired
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id"); // insert문을 자동으로 만들어줌
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
+        return result.stream().findAny();  //?
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member", memberRowMapper());
+    }
+
+
+    private RowMapper<Member> memberRowMapper() {
+//        return new RowMapper<Member>() {  // option + enter 로 람다로 바꿀수있다
+//            @Override
+//            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                Member member = new Member();
+//                member.setId(rs.getLong("id"));
+//                member.setName(rs.getString("name"));
+//                return member;
+//            }
+//        };
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+
+}
--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring.service;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,8 @@ public class SpringConfig {
     public MemberRepository memberRepository() {  // MemberRepository : interface
         //return new MemoryMemberRepository(); // MemoryMemberRepository : 구현체
         // config를통한 방법은 컴포넌트 스캔 방법에 비해 구현체를 바꾸기가 쉽다    return new DbMemberRepository();
-        return new JdbcMemberRepository(dataSource); // 다형성을 활용 , 개방-폐쇄 원칙
+        //return new JdbcMemberRepository(dataSource); // 다형성을 활용 , 개방-폐쇄 원칙 , 순수jdbc
+        return new JdbcTemplateMemberRepository(dataSource);// jdbc 템플릿
     }
 
 
