1b1da4f543b60fc7db9e7b1babe872313fccb3e2
jason <ypc1@naver.com>
2022-11-21 21:57:30 +0900

    H2 데이터 베이스 이용한 순수JDBC 코드

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,12 +1,24 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    //@Autowired DataSource dataSource;
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,8 +27,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {  // MemberRepository : interface
-        return new MemoryMemberRepository(); // MemoryMemberRepository : 구현체
+        //return new MemoryMemberRepository(); // MemoryMemberRepository : 구현체
         // config를통한 방법은 컴포넌트 스캔 방법에 비해 구현체를 바꾸기가 쉽다    return new DbMemberRepository();
+        return new JdbcMemberRepository(dataSource); // 다형성을 활용 , 개방-폐쇄 원칙
     }
 
 
